762b196312a43435c943bad5400b77d39630aad4
hannidinh <44892850+hannidinh@users.noreply.github.com>
2025-05-29 08:06:06 -0500

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hard works beat talents</Button>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant="elevated">Hard works</Button>
+      </div>
+      <div>
+        <Input placeholder="Type here..." />
+      </div>
+      <div>
+        <Progress />
+      </div>
+      <div>
+        <Textarea></Textarea>
+      </div>
     </div>
   );
 }
