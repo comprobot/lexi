1343c07d4761e3fca7d0c536f2d98f429d44a3f2
hannidinh <44892850+hannidinh@users.noreply.github.com>
2025-06-16 18:39:15 -0500

    14: product list ui

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,32 +1,31 @@
-import {
-  BookList,
-  BookListSkeleton,
-} from "@/modules/books/ui/components/book-list";
+import { loadBookFilters } from "@/modules/books/search-params";
+import { BookListView } from "@/modules/books/ui/views/book-list-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { SearchParams } from "nuqs/server";
 
 interface Props {
   params: Promise<{
     subcategory: string;
   }>;
+  searchParams: Promise<SearchParams>;
 }
 
-const Page = async ({ params }: Props) => {
+const Page = async ({ params, searchParams }: Props) => {
   const { subcategory } = await params;
+  const filters = await loadBookFilters(searchParams);
 
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(
     trpc.books.getMany.queryOptions({
       category: subcategory,
+      ...filters,
     })
   );
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<BookListSkeleton />}>
-        <BookList category={subcategory} />
-      </Suspense>
+      <BookListView category={subcategory} />
     </HydrationBoundary>
   );
 };
--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,14 +1,9 @@
-import { BookFilters } from "@/modules/books/ui/components/book-filters";
-import {
-  BookList,
-  BookListSkeleton,
-} from "@/modules/books/ui/components/book-list";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
-import { Suspense } from "react";
+
 import type { SearchParams } from "nuqs/server";
 import { loadBookFilters } from "@/modules/books/search-params";
-import { BookSort } from "@/modules/books/ui/components/book-sort";
+import { BookListView } from "@/modules/books/ui/views/book-list-view";
 
 interface Props {
   params: Promise<{
@@ -31,24 +26,7 @@ const Page = async ({ params, searchParams }: Props) => {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
-        <div className="flex flex-col lg:flex-row lg:items-center gap-y-2 lg:gap-y-0 justify-between">
-          <p className="text-2xl font-medium">Curated for you</p>
-          <BookSort />
-        </div>
-
-        <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
-          <div className="lg:col-span-2 xl:col-span-2">
-            <BookFilters />
-          </div>
-
-          <div className="lg:col-span-4 xl:col-span-6">
-            <Suspense fallback={<BookListSkeleton />}>
-              <BookList category={category} />
-            </Suspense>
-          </div>
-        </div>
-      </div>
+      <BookListView category={category} />
     </HydrationBoundary>
   );
 };
