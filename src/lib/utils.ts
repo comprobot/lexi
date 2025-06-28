af1bb6fa28a8b64e9fd36a6b8b6045fbe3524ebd
hannidinh <44892850+hannidinh@users.noreply.github.com>
2025-06-28 09:04:44 -0500

    28-3: fix deployment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,9 +5,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// In development mode, use normal routing
 export function generateTenantURL(tenantSlug: string) {
-  if (process.env.NODE_ENV === "development") {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = Boolean(
+    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING
+  );
+
+  // In development or subdomain routing disabled mode, use normal routing
+  if (isDevelopment || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
 
